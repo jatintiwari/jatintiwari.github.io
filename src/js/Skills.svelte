0d4c8f33da63e9f57f0c305edc5e73efce0570f4
Jatin <mail@jatintiwari.com>
2022-04-27 09:17:11 +0530

    Update Skills.svelte

--- src/js/Skills.svelte
+++ src/js/Skills.svelte
@@ -8,18 +8,18 @@
 <section class="section">
     <section class="container">
         <p>
-            I have ~{yearOfExperience} years of experience in frontend development. For ~5.5 years I have worked with e-commerce companies.
+            I have {yearOfExperience}+ years of experience in frontend development. For approximately 5 years I have worked with small to very large scale e-commerce companies.
         </p>
         <p>I have advance knowledge in Web and App development.</p>
         <p>
-            I have expertise in <span class="text-bold">Javascript</span>. These days I am using
-            <span class="highlight">React JS</span>. I also have prior experience with <span class="highlight">Angular JS</span> and <span class="highlight">Backbone JS</span>.
+            I have expertise in <span class="text-bold">Javascript</span>. I have started working with
+            <span class="highlight">React JS</span> during my days with <a class="cta" href="https://en.wikipedia.org/wiki/Furlenco" target="_blank">Furlenco</a> and instantly liked it. I also have prior experience with <span class="highlight">Angular JS</span> and <span class="highlight">Backbone JS</span>.
         </p>
         <p>
             I am enthusiastic about <span class="highlight">Puppeteer JS</span> and can very quickly build a scalable API in
             <span class="highlight">NodeJS</span>. While working with
             <a class="cta" href="https://en.wikipedia.org/wiki/Flipkart" target="_blank">Flipkart</a>, I had created a Visual Regression tool which
-            was able to take screenshots of all the app workflows and then assert baseline with checkpoint images.
+            was able to take screenshots of all the app workflows and then assert baseline with checkpoint images. This tool solved problems involved with manual testing.
         </p>
         <p>
             I ❤️ <span class="text-italic">building apps from scratch</span> and then scale them to serve millions of cusomters. I also believe
