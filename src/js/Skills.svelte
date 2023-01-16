72e76724e86d74bc7f121abc1c013fc9ea397f1f
Jatin Tiwari ̰ <mail@jatintiwari.com>
2023-01-16 08:46:30 +0530

    mishtakes

--- src/js/Skills.svelte
+++ src/js/Skills.svelte
@@ -8,25 +8,25 @@
 <section class="section">
     <section class="container">
         <p>
-            I have {yearOfExperience}+ years of experience in frontend development. For approximately 5 years I have worked with small to very large scale e-commerce companies.
+            I have {yearOfExperience}+ years of experience in front-end development. For approximately {yearOfExperience-1} years I have worked with small to very large-scale e-commerce companies.
         </p>
-        <p>I have advance knowledge in Web and App development.</p>
+        <p>I have advanced knowledge in Web and App development.</p>
         <p>
-            I have expertise in <span class="text-bold">Javascript</span>. I have started working with
+            I have expertise in <span class="text-bold">Javascript</span>. I started working with
             <span class="highlight">React JS</span> during my days with <a class="cta" href="https://en.wikipedia.org/wiki/Furlenco" target="_blank">Furlenco</a> and instantly liked it. I also have prior experience with <span class="highlight">Angular JS</span> and <span class="highlight">Backbone JS</span>.
         </p>
         <p>
             I am enthusiastic about <span class="highlight">Puppeteer JS</span> and can very quickly build a scalable API in
             <span class="highlight">NodeJS</span>. While working with
-            <a class="cta" href="https://en.wikipedia.org/wiki/Flipkart" target="_blank">Flipkart</a>, I had created a Visual Regression tool which
+            <a class="cta" href="https://en.wikipedia.org/wiki/Flipkart" target="_blank">Flipkart</a>, I created a Visual Regression tool that
             was able to take screenshots of all the app workflows and then assert baseline with checkpoint images. This tool solved problems involved with manual testing.
         </p>
         <p>
-            I ❤️ <span class="text-italic">building apps from scratch</span> and then scale them to serve millions of customers. I also believe
-            that it takes time to build such apps and use of technology is as per need basis.
+            I ❤️ <span class="text-italic">building apps from scratch</span> and then scaling them to serve millions of customers. I also believe
+            that it takes time to build such apps and use of technology is on as per need basis.
         </p>
         <p>
-            I have helped a few friends to kick off their startup dream with native apps in <span class="highlight">Flutter</span>
+            I helped a few friends to kick off their startup dream with native apps in <span class="highlight">Flutter</span>
             and <span class="highlight">React native</span>.
         </p>
     </section>
